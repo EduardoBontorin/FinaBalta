56e04e2eb4a5dec06b061034c077c925abbda27c
EduardoBontorin <eduardo.bontorin@outlook.com>
2024-06-05 17:23:23 -0300

    Finalizado modulo Core

---
+++ Fina.Core/Requests/Transactions/UpdateTransactionRequest.cs
@@ -0,0 +1,29 @@
+﻿using Fina.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fina.Core.Requests.Transactions
+{
+    public class UpdateTransactionRequest : Request
+    {
+        public long Id { get; set; }
+        [Required(ErrorMessage = "Título inválido")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Tipo inválido")]
+        public ETransactionType Type { get; set; }
+
+        [Required(ErrorMessage = "Valor inválido")]
+        public decimal Amount { get; set; }
+
+        [Required(ErrorMessage = "Categoria Inválida")]
+        public long CategoryId { get; set; }
+
+        [Required(ErrorMessage = "Data inválida")]
+        public DateTime? PaidOrReceivedAt { get; set; }
+    }
+}
